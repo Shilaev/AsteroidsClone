935a480c31df43112eb7f11d4ba78aea135ac24d
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-02-08 20:52:09 +0300

    refactor Pool class

--- Assets/Scripts/ObjectPool/Pool/Pool.cs
+++ Assets/Scripts/ObjectPool/Pool/Pool.cs
@@ -7,20 +7,29 @@ namespace ObjectPool
     [AddComponentMenu("Object Pool/Pool")]
     public class Pool : MonoBehaviour
     {
-        private static readonly List<Pool> _pools = new List<Pool>();
+        #region List_of_pools_fields
         [SerializeField] private List<Pool_SO> _pools_so = new List<Pool_SO>();
+        private static readonly List<Pool> _pools = new List<Pool>();
+        #endregion
 
-        private List<PoolElement> _elements;
+        #region Single_pool_fields
+        private string _tag;
+        private int _size;
         private bool _isAutoExpand;
         private PoolElement _poolElement;
-        private int _size;
-        private string _tag;
+        private List<PoolElement> _poolElements;
+        #endregion
 
+        #region PrivateMethods
         private void Start()
         {
             Initialise();
         }
 
+        /// <summary>
+        /// Create and prepare all pools,
+        /// using scriptable object pool templates
+        /// </summary>
         private void Initialise()
         {
             foreach (var poolSo in _pools_so)
@@ -31,13 +40,20 @@ namespace ObjectPool
             }
         }
 
+        /// <summary>
+        /// Add new pool in list of pools
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <param name="size"></param>
+        /// <param name="isAutoExpand"></param>
+        /// <param name="poolElement"></param>
         private void AddPool(string tag, int size, bool isAutoExpand, PoolElement poolElement)
         {
             _tag = tag;
             _size = size;
             _isAutoExpand = isAutoExpand;
             _poolElement = poolElement;
-            _elements = new List<PoolElement>();
+            _poolElements = new List<PoolElement>();
 
             foreach (var poolSo in _pools_so)
             {
@@ -49,14 +65,32 @@ namespace ObjectPool
             for (var i = 0; i < _size; i++) AddElement(_poolElement);
         }
 
+        /// <summary>
+        /// Add element in pool
+        /// </summary>
+        /// <param name="poolElement"></param>
         private void AddElement(PoolElement poolElement)
         {
-            var temp = Instantiate(poolElement);
-            temp.gameObject.SetActive(false);
-            _elements.Add(temp);
+            var element = Instantiate(poolElement);
+            element.gameObject.SetActive(false);
+            _poolElements.Add(element);
         }
 
-        public static GameObject SpawnPoolObjectWithTag(string tag)
+        private PoolElement GetFreeElement()
+        {
+            foreach (var poolElement in _poolElements)
+                if (poolElement.gameObject.activeInHierarchy == false)
+                    return poolElement;
+
+            if (_isAutoExpand)
+                AddElement(_poolElements[0]);
+
+            throw new Exception($"No elements in {_tag} pool");
+        }
+        #endregion
+
+        #region PublicMethods
+        public static GameObject GetElementFromPoolWithTag(string tag)
         {
             foreach (var pool in _pools)
                 if (pool._tag == tag)
@@ -72,17 +106,6 @@ namespace ObjectPool
 
             throw new Exception($"pool with tag '{tag}' is empty.");
         }
-
-        public PoolElement GetFreeElement()
-        {
-            foreach (var poolElement in _elements)
-                if (poolElement.gameObject.activeInHierarchy == false)
-                    return poolElement;
-
-            if (_isAutoExpand)
-                AddElement(_elements[0]);
-
-            throw new Exception($"No elements in {_tag} pool");
-        }
+        #endregion
     }
 }
