1d87edb7b109a75cddc869c2038dad530aede4e0
Shilaev Valentin <valentin.shilaev@yandex.ru>
2022-02-11 08:07:21 +0300

    workable beta 1.0

---
+++ Assets/Scripts/Asteroid/Controllers/AsteroidSpawner.cs
@@ -0,0 +1,26 @@
+using ObjectPool;
+using UnityEngine;
+
+[AddComponentMenu("Controllers/AsteroidSpawner")]
+public class AsteroidSpawner : MonoBehaviour
+{
+    public static void SpawnAsteroid(int amount, string type, Vector3 spawnPoint)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            var newAsteroid = Pool.GetFreeElementFromPool(type);
+            newAsteroid.gameObject.SetActive(true);
+            newAsteroid.transform.position = spawnPoint;
+            SetUpNewAsteroid(newAsteroid);
+        }
+    }
+
+    private static void SetUpNewAsteroid(GameObject newAsteroid)
+    {
+        var randomForceDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        var randomForceValue = Random.Range(50f, 200f);
+        newAsteroid.GetComponent<Rigidbody2D>().AddForce(randomForceDirection * randomForceValue, ForceMode2D.Force);
+
+        newAsteroid.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+    }
+}
